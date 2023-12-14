473922dcefbd278b534eb90cbb87ef4aeab150ec
moriarty <nacud85@gmail.com>
2023-12-14 19:40:11 +0300

    changed Mapper

--- app/src/main/java/com/example/weatherapp/domain/mapper/WeatherMapper.kt
+++ app/src/main/java/com/example/weatherapp/domain/mapper/WeatherMapper.kt
@@ -3,14 +3,15 @@ package com.example.weatherapp.domain.mapper
 import com.example.weatherapp.domain.model.CurrentWeatherCardModel
 import com.example.weatherapp.domain.model.DaysWeatherItemModel
 import com.example.weatherapp.data.model.response.WeatherResponse
+import kotlin.math.floor
 
 fun WeatherResponse.toCurrentCardWeather() = CurrentWeatherCardModel(
     nameCity = this.location?.name,
     dateTime = this.location?.localtime,
     conditionText = this.current?.condition?.text,
-    currentTemp = this.current?.tempC,
-    maxTemp = this.forecast?.forecastDay?.get(0)?.day?.maxTempC,
-    minTemp = this.forecast?.forecastDay?.get(0)?.day?.minTempC,
+    currentTemp = this.current?.tempC?.let { it1 -> floor(it1) },
+    maxTemp = this.forecast?.forecastDay?.first()?.day?.maxTempC?.let { it1 -> floor(it1) },
+    minTemp = this.forecast?.forecastDay?.first()?.day?.minTempC?.let { it1 -> floor(it1) },
     imageUrl = this.current?.condition?.icon
 )
 
@@ -19,8 +20,8 @@ fun WeatherResponse.toDaysItemWeatherList(): List<DaysWeatherItemModel>? {
         DaysWeatherItemModel(
             dateTime = it.date,
             conditionText = it.day?.condition?.text,
-            maxTemp = it.day?.maxTempC,
-            minTemp = it.day?.minTempC,
+            maxTemp = it.day?.maxTempC?.let { it1 -> floor(it1) },
+            minTemp = it.day?.minTempC?.let { it1 -> floor(it1) },
             imageUrl = it.day?.condition?.icon
         )
     }
